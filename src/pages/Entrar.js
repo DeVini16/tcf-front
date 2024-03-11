e1468274562d8e5f1b5f8fc3eec347d7ab989b31
matheus alves formiga <formiga_matheus@hotmail.com>
2024-03-10 22:47:22 -0300

    CSS da pagina entrar

--- src/pages/Entrar.js
+++ src/pages/Entrar.js
@@ -19,20 +19,24 @@ const Entrar = () => {
 
   return (
     <div>
-      <form action="">
-      <div className='container-entrar'>
+      <div className='container'>
+        <form action="">
+        <div className='container-entrar'>
 
-        <h1 className='h1-login'>Falta pouco para matar sua fome!</h1>
-        <h2 className='h2-login'>Como deseja continuar?</h2>
+          <h1 className='h1-login'>Falta pouco para matar sua fome!</h1>
+          <h2 className='h2-login'>Como deseja continuar?</h2>
 
-        <button className='btn-facebook' onClick={buttonResponse}>Continuar com Facebook</button>
-        <button className='btn-google' onClick={buttonResponse}>Fazer login com o Google</button>
-
-        <button className='btn-celular' onClick={buttonResponse}>Celular</button>
-        <button className='btn-email' onClick={buttonResponse}>E-mail</button>
-
+          <button className='btn-facebook' onClick={buttonResponse}>Continuar com Facebook</button>
+          <button className='btn-google' onClick={buttonResponse}>Fazer login com o Google</button>
+          <div className='cel-email'>
+            <button className='btn-celular' onClick={buttonResponse}>Celular</button>
+            <button className='btn-email' onClick={buttonResponse}>E-mail</button>
+          </div>
+          <div className='linha'>
+          </div>
+        </div>
+        </form>
       </div>
-      </form>
     </div>
   )
 }
